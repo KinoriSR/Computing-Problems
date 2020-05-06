410db6b0af82941022c4e18698995a4786a6d5a4
Kinori <46947508+KinoriSR@users.noreply.github.com>
2020-05-06 14:13:53 -0400

    Add files via upload

---
+++ BinarySearch.py
@@ -0,0 +1,18 @@
+#find where a number would go in an array
+
+def binary_search(n,L):
+    if len(L)==1 and L[0]==n:
+        return 0
+    elif len(L)==1 and L[0]<n:
+        return 1
+    elif len(L)==1 and L[0]>n:
+        return 0
+    half=len(L)//2
+    if n<L[half]:
+        return binary_search(n,L[:half])
+    else:
+        return half+binary_search(n,L[half:])
+
+
+L=[1,2,3,5,6]
+print(binary_search(3,L))
