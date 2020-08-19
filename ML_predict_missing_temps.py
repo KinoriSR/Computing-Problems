7d4ff24f718a2be3c9f9eced9469afb07362506a
Kinori <46947508+KinoriSR@users.noreply.github.com>
2020-08-19 12:08:49 -0400

    Update and rename predict_missing_temps.py to ML_predict_missing_temps.py

--- ML_predict_missing_temps.py
+++ ML_predict_missing_temps.py
@@ -21,7 +21,7 @@ data.replace('None', np.NaN, inplace=True)
 # transform data
 month = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 data['yyyy']=pd.to_numeric(data['yyyy'])
-data['month'].replace(month, inplace=True) # can also try one hot encoding
+data['month'].replace(month, inplace=True) # can also try one hot encoding, but there is ordering
 data['tmax']=pd.to_numeric(data['tmax'])
 data['tmin']=pd.to_numeric(data['tmin'])
 
